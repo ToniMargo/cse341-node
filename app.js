2466838a9a7bf980d063f05b4afe71a280833a75
antoniomartinez <mar20126@byui.edu>
2023-05-20 08:53:14 -0600

    Moved .env to root folder and added index.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const mongodb = require("./models/connect");
-const contactsRoutes = require("./routes/contacts");
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -13,7 +12,7 @@ app
     res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   })
-  .use("/contacts", contactsRoutes);
+  .use("/", require("./routes"));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
