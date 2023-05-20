3f8b7d9a1d4e73a52fbab4a4c7f2e4cdf02b765c
antoniomartinez <mar20126@byui.edu>
2023-05-20 13:55:02 -0600

    Deleted get request that was messing with Swagger

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
-
-router.get('/', (req, res) => {
-  res.send('Vianney Alcala');
-});
-
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
