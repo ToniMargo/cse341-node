c4abe78730e1349494b0fd22e46f2141b783925f
antoniomartinez <mar20126@byui.edu>
2023-05-20 08:55:36 -0600

    FIxed some routing errors

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,8 +1,6 @@
-const express = require("express");
+const router = require("express").Router();
 
 const contactsController = require("../controllers/contacts");
-
-const router = express.Router();
 
 // GET /feed/posts
 router.get("/", contactsController.getData);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send("Vianney Alcala");
+});
 
 router.use("/contacts", require("./contacts"));
 
